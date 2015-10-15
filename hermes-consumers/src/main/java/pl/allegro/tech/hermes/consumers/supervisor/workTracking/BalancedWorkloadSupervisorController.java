d6091d13b0a4f783014a864724bb5072da8482e3
wojtkiewicz <wojtkiewicz.bartosz@gmail.com>
2015-10-15 13:09:46 +0200

    204 getting started with tests for consumers registry

---
+++ hermes-consumers/src/main/java/pl/allegro/tech/hermes/consumers/supervisor/workTracking/BalancedWorkloadSupervisorController.java
@@ -0,0 +1,59 @@
+package pl.allegro.tech.hermes.consumers.supervisor.workTracking;
+
+import com.google.common.collect.ImmutableList;
+import pl.allegro.tech.hermes.api.Subscription;
+import pl.allegro.tech.hermes.api.SubscriptionName;
+import pl.allegro.tech.hermes.consumers.subscription.cache.SubscriptionsCache;
+import pl.allegro.tech.hermes.consumers.supervisor.ConsumersSupervisor;
+
+public class BalancedWorkloadSupervisorController implements SupervisorController {
+    private ConsumersSupervisor supervisor;
+    private SubscriptionsCache subscriptionsCache;
+    private WorkTracker workTracker;
+    private ConsumersRegistry consumersRegistry;
+
+    public BalancedWorkloadSupervisorController(ConsumersSupervisor supervisor, SubscriptionsCache subscriptionsCache, WorkTracker workTracker, ConsumersRegistry consumersRegistry) {
+        this.supervisor = supervisor;
+        this.subscriptionsCache = subscriptionsCache;
+        this.workTracker = workTracker;
+        this.consumersRegistry = consumersRegistry;
+    }
+
+    @Override
+    public void onSubscriptionCreated(Subscription subscription) {
+
+    }
+
+    @Override
+    public void onSubscriptionRemoved(Subscription subscription) {
+
+    }
+
+    @Override
+    public void onSubscriptionChanged(Subscription subscription) {
+
+    }
+
+    @Override
+    public void onSubscriptionAssigned(Subscription subscription) {
+
+    }
+
+    @Override
+    public void onAssignmentRemoved(SubscriptionName subscriptionName) {
+
+    }
+
+    @Override
+    public void start() throws Exception {
+        subscriptionsCache.start(ImmutableList.of(this));
+        workTracker.start(ImmutableList.of(this));
+        supervisor.start();
+        consumersRegistry.register();
+    }
+
+    @Override
+    public void shutdown() throws InterruptedException {
+        supervisor.shutdown();
+    }
+}
---
+++ hermes-consumers/src/main/java/pl/allegro/tech/hermes/consumers/supervisor/workTracking/ConsumersRegistry.java
@@ -0,0 +1,11 @@
+package pl.allegro.tech.hermes.consumers.supervisor.workTracking;
+
+public class ConsumersRegistry {
+    public void register() {
+
+    }
+
+    public boolean isRegistered(String supervisorId) {
+        return false;
+    }
+}
